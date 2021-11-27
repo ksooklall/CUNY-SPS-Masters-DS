b582d749608ad177c52c3db06c1c3bbc0c1af893
kenan <ksooklall@arlo.com>
2021-11-27 15:06:14 -0500

    602 clean up

--- DATA_602/store_sales.py
+++
@@ -1,6 +0,0 @@
-sales = []
-
-for i in ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']:
-    sales.append(int(input('{} sales'.format(i))))
-
-print('Weekly sales: {}'.format(sum(sales)/len(sales)))
